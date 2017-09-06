cb69865ab7d6cddfd6f7282767d5ae3d54cb78d4
Jason Bellows <bellowsj@gmail.com>
2017-09-06 16:06:14 -0600

    support for goman_hashing

--- aiopogo/hash_server.py
+++ aiopogo/hash_server.py
@@ -16,6 +16,7 @@ from .utilities import f2i
 class HashServer:
     _session = None
     multi = False
+    goHash = False
     loop = get_event_loop()
     status = {}
     log = getLogger('hashing')
@@ -38,11 +39,19 @@ class HashServer:
                     iteration += 1
                 else:
                     self.log.info('Out of hashes, waiting for new period.')
-                    await sleep(status['refresh'] - time() + 1, loop=self.loop)
-                    break
+                    if not self.goHash:
+                        await sleep(status['refresh'] - time() + 1, loop=self.loop)
+                        break
+                    # Go Hash doesn't have an expiry period so you would top up credit, we will try again in 5 seconds
+                    else:
+                        await sleep(5, loop=self.loop)
+                        break
         except KeyError:
             pass
         headers = {'X-AuthToken': self.instance_token}
+        if self.goHash:
+             # extra header ensures no more than 5000 hashes per minute when using Go Hash. You can up this if your CPU can handle it
+             headers = {'X-AuthToken': self.instance_token, 'X-RateLimit':'50000000'}
 
         payload = {
             'Timestamp': timestamp,
@@ -57,7 +66,7 @@ class HashServer:
         # request hashes from hashing server
         for attempt in range(3):
             try:
-                async with self._session.post("http://pokehash.buddyauth.com/api/v143_1/hash", headers=headers, json=payload) as resp:
+                async with self._session.post(self.endPointUrl, headers=headers, json=payload) as resp:
                     if resp.status == 400:
                         status['failures'] += 1
 
@@ -91,8 +100,19 @@ class HashServer:
                 elif e.code == 429:
                     status['remaining'] = 0
                     self.instance_token = self.auth_token
+                    if e.code == 429:
+                        if self.goHash:
+                            self.log.warning("Error 429 - Artificial hash limit reached, consider a higher value for X-RateLimit header in Go Hash request.")
+                        else:
+                            self.log.warning("Error 429 - Out of hashes for this period.")
+                    else:
+                        self.log.warning("Error 430 - No credit remaining on the Go Hash key.")
                     return await self.hash(timestamp, latitude, longitude, accuracy, authticket, sessiondata, requests)
                 elif e.code >= 500 or e.code == 404:
+                    if e.code == 503 and self.goHash:
+                        self.log.warning("Error 503 - Go Hash server cannot handle the load.")
+                    if e.code == 549 or e.code == 550 and self.goHash:
+                        self.log.warning("Error 549|550 something bad happened between Bossland and Go Hash not successful after multiple retries.")
                     raise HashingOfflineException(
                         'Hashing server error {}: {}'.format(
                             e.code, e.message))
@@ -140,9 +160,15 @@ class HashServer:
         return self.key_statuses[self.instance_token]
 
     @classmethod
-    def activate_session(cls, conn_limit=300):
+    def activate_session(cls, conn_limit=300, go_hash=False):
+        cls.goHash = go_hash
         if cls._session and not cls._session.closed:
             return
+        if cls.goHash:
+            cls.endPointUrl = "http://hash.goman.io/api/v137_1/hash"
+            cls.log.warning("Hash server set to Go Hash mode. Please ensure you are using a Go Hash NOT a Bossland hash key.")
+        else:
+            cls.log.warning("Hash server set to Bossland mode. Please ensure you are using a Bossland NOT a Go Hash hash key.")
         conn = TimedConnector(loop=cls.loop,
                               limit=conn_limit,
                               verify_ssl=False)
