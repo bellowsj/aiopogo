e05ed87983d07814530ccc5b452e01774250cfa3
Monkey Ted Baker <monkey.ted.baker@gmail.com>
2017-09-06 01:08:24 +0000

    [fix]goman endpoints

--- aiopogo/hash_server.py
+++ aiopogo/hash_server.py
@@ -20,7 +20,7 @@ class HashServer:
     loop = get_event_loop()
     status = {}
     log = getLogger('hashing')
-    endPointUrl = "http://pokehash.buddyauth.com/api/v137_1/hash"
+    endPointUrl = "http://pokehash.buddyauth.com/api/v143_1/hash"
 
     def __init__(self):
         try:
@@ -167,8 +167,8 @@ class HashServer:
         if cls._session and not cls._session.closed:
             return
         if cls.goHash:
-            #cls.endPointUrl = "http://hash.goman.io/api/v137_1/hash"
-            cls.endPointUrl = "http://hash.gomanager.biz/api/v137_1/hash"
+            #cls.endPointUrl = "http://hash.goman.io/api/v143_1/hash"
+            cls.endPointUrl = "http://hash.gomanager.biz/api/v143_1/hash"
             cls.log.warning("Hash server set to Go Hash mode. Please ensure you are using a Go Hash NOT a Bossland hash key.")
         else:
             cls.log.warning("Hash server set to Bossland mode. Please ensure you are using a Bossland NOT a Go Hash hash key.")
